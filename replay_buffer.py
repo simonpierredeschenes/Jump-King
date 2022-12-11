0b2f24a325397ea6dccda59fbd7c31a9c9b3f762
PatriceLajoie <106343046+PatriceLajoie@users.noreply.github.com>
2022-12-11 10:26:09 -0500

    priorization

--- replay_buffer.py
+++ replay_buffer.py
@@ -3,10 +3,11 @@ import random
 
 
 class ReplayBuffer:
-    def __init__(self, buffer_size):
+    def __init__(self, buffer_size,epsilon_P=0.4):
         self.__buffer_size = buffer_size
         self.__permanent_buffer = deque()
         self.__volatile_buffer = deque()
+        self.epsilon_Priorization=epsilon_P
 
     def store(self, element, permanent=False):
         if permanent:
@@ -18,13 +19,17 @@ class ReplayBuffer:
             self.__volatile_buffer.popleft()
 
     def get_batch(self, batch_size):
-        return random.sample(self.__permanent_buffer + self.__volatile_buffer, batch_size)
+        sample_Volatile=random.sample(self.__volatile_buffer, round((1-self.epsilon_Priorization)*batch_size))
+        sample_Permanent=random.sample(self.__permanent_buffer, round((self.epsilon_Priorization)*batch_size))
+        list=sample_Permanent+sample_Volatile
+        random.shuffle(list)
+        return list
 
     def get_buffer(self):
         return self.__permanent_buffer + self.__volatile_buffer
 
     def get_size(self):
-        return len(self.__permanent_buffer) + len(self.__volatile_buffer)
+        return min(len(self.__permanent_buffer),len(self.__volatile_buffer))
 
     def __getitem__(self, index):
         return (self.__permanent_buffer + self.__volatile_buffer)[index]
